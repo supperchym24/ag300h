cba4af719da0444732094d3a6e636e38339b9d28
supperchym24 <97427356+supperchym24@users.noreply.github.com>
2022-01-10 12:57:22 +0700

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'src-git kenzo https://github.com/supperchym23/openwrt-packages.git' >>feeds.conf.default
+echo 'src-git kenzosmall https://github.com/kenzok8/small.git' >>feeds.conf.default
+echo 'src-git supperchym https://github.com/supperchym23/openwrtpackage.git' >>feeds.conf.default
